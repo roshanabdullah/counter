471968acf40cb7d931703d5726ca39aefb581636
roshanabdullah <abdullahroshan55@gmil.com>
2022-05-29 20:55:55 +0500

    counter

---
+++ src/components/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+function App() {
+  var [count, setCount] = useState(0);
+
+  function increase(e) {
+    e.preventDefault();
+    setCount(++count);
+  }
+  function decrease(e) {
+    e.preventDefault();
+    setCount(--count);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
